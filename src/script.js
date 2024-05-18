000a0ce650b0169810bddbfb77c967ee91f56592
Daniel <danielbreu.sar@gmail.com>
2024-05-18 01:23:36 -0300

    Primeiro commit

---
+++ src/script.js
@@ -0,0 +1,48 @@
+//# 1️⃣ Desafio Classificador de nível de Herói
+
+
+let nome = "Daniel"
+let xp = 7362
+let nivel = ""
+
+
+switch (true) {
+  case (xp < 1000):
+    nivel = "Ferro"
+    break;
+    
+  case ((xp >=1001) && (xp <= 2000)):
+    nivel = "Bronze"
+    break;
+    
+  case ((xp >= 2001) && (xp <= 5000)):
+    nivel = "Prata"
+    break;
+    
+  case ((xp >= 5001) && (xp <= 7000)):
+    nivel = "Ouro"
+    break;
+    
+  case ((xp >= 7001) && (xp <= 8000)):
+    nivel = "Platina"
+    break;
+    
+  case ((xp >=8001) && (xp <= 9000)):
+    nivel = "Ascendente"
+    break;
+    
+  case ((xp >= 9001) && (xp <= 10000)):
+    nivel = "Imortal"
+    break;
+    
+  case (xp >= 10001):
+    nivel = "Radiante"
+    break;
+}
+
+console.log("O herói de nome " + nome + " Está no nível de: " + nivel)
+
+
+
+
+
